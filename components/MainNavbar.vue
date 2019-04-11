883258e10e713c358e3de1f81d229464185bf80c
raramdha <ramdhaidfitri.martmis@ui.ac.id>
2019-04-12 00:38:53 +0700

    final

--- components/MainNavbar.vue
+++ components/MainNavbar.vue
@@ -52,13 +52,10 @@
 <script> 
 let resizeTimeout;
 function resizeThrottler(actualResizeHandler) {
-  // ignore resize events as long as an actualResizeHandler execution is in the queue
   if (!resizeTimeout) {
     resizeTimeout = setTimeout(() => {
       resizeTimeout = null;
       actualResizeHandler();
-
-      // The actualResizeHandler will execute at a rate of 15fps
     }, 66);
   }
 }
